8544e978f820089d6e5dacbd15b065be40e2a957
KhankSky <pkhanhsnd037@gmail.com>
2025-04-17 12:24:07 +0700

    first commit

---
+++ motogp_b/src/main/java/com/example/motogp_b/MotogpBApplication.java
@@ -0,0 +1,13 @@
+package com.example.motogp_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotogpBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotogpBApplication.class, args);
+	}
+
+}
---
+++ motogp_b/src/test/java/com/example/motogp_b/MotogpBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.motogp_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotogpBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
